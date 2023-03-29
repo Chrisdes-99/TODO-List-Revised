61e8c65ed0b31856b55238c2b1540f437ec0c62f
Christopher De Santiago <chris@ChristophersMBP.lan>
2023-03-29 12:13:48 -0700

    basic UI and functionality finished

--- index.js
+++ index.js
@@ -1,94 +1,94 @@
 let tasks = [];
+const countTracker = document.querySelector('.count');
 
+// Fires up function to add task to array, display cards and reset input values
 document.getElementById("submitForm").addEventListener("click", function(event){
     event.preventDefault();
     addTasktoList();
     const task = document.getElementById("taskName");
     const description = document.getElementById("taskDescription");
     const priority = document.getElementById("taskPriority");
-    const check = document.getElementById("checkBox");
+    //const check = document.getElementById("checkBox");
 
     task.value = "";
     description.value = "";
     priority.value = 1;
 
-    check.checked = false;
+    //check.checked = false;
   });
 
+
+// Task Object
 function Task() {
     this.task = document.getElementById("taskName").value;
     this.description = document.getElementById("taskDescription").value;
     this.priority = document.getElementById("taskPriority").value;
-    this.started = document.getElementById("checkBox").value;
+    this.dueDate = document.getElementById("date").value;
+    this.progress = document.querySelectorAll('option:checked').value;
+    //this.started = document.getElementById("checkBox").value;
   }
 
-  function addTasktoList(){
-    let newTask = new Task();
+// User Input but into an object which is pushed into task array
+function addTasktoList(){
+ let newTask = new Task();
 
-    tasks.push(newTask);
+ tasks.push(newTask);
 
-    taskCard(tasks);
+ console.log(tasks);
 
-    taskCount();
-
-    console.log(tasks);
+ taskCard(tasks);
 }
 
-function taskCount(){
-    const countTracker = document.querySelector('.count');
 
-    countTracker.textContent = tasks.length;
-
-    return countTracker;
-}
-
+//Creates a New Card Upon User Submit
 function taskCard(taskArray){
+    let count = 0;
+
     const cards = document.querySelector('.cards');
-    const startedOrNot = document.querySelector('#checkBox');
+
     const priority = document.getElementById("taskPriority");
 
     const newCard = document.createElement('div');
     newCard.className = "newCard"
 
+    //Displayes Input on New Card
     const taskHeader = document.createElement('h4');
     const descriptionHeader = document.createElement('p');
     const priorityHeader = document.createElement('p');
-    const startedOrNotMessage = document.createElement('p');
-    const date = document.createElement('p');
-    date.textContent = `Posted: ${postDate()}`
+    const due = document.createElement('p');
+    //due.style.textDecoration = "underline";
+    due.style.fontWeight = "bold";
+    
+
+    // New Button inside New Card
     const newButton = document.createElement('button');
-    newButton.innerHTML = "Remove"
+    newButton.innerHTML = "Remove";
+    newButton.className = "newButton";
 
     const line = document.createElement('hr');
 
-    //const anotherCheckBox = document.createElement('input');
-    //anotherCheckBox.type = "checkbox";
-
-    //const newButton = document.createElement('button');
-    //newButton.className = "removeButton";
-
-
+    // Goes through task array to grab and Display Object Data
     for(let t of taskArray){
         taskHeader.textContent = `${t.task}`;
         descriptionHeader.textContent = `${t.description}`;
         priorityHeader.textContent = `${t.priority}`;
+        due.textContent = `Due: ${t.dueDate}`;
     }
 
-    validate(startedOrNot,startedOrNotMessage);
+    //validate(startedOrNot,startedOrNotMessage);
     priorityCheck(priority.value,newCard);
 
     cards.appendChild(newCard);
     newCard.appendChild(taskHeader);
     newCard.appendChild(descriptionHeader);
-    newCard.appendChild(startedOrNotMessage);
-    newCard.appendChild(newButton)
+    //newCard.appendChild(startedOrNotMessage);
+    newCard.appendChild(due);
     newCard.appendChild(line);
-    newCard.appendChild(date);
+    newCard.appendChild(newButton);
+    //newCard.appendChild(date);
 
     newButton.addEventListener('click',()=>{
         newButton.parentNode.remove();
-        delete tasks[newButton.parentNode];
-        tasks.length -= 1;
     })
 }
 
@@ -106,28 +106,42 @@ function validate(check,text){
     }
 }
 
+// Changes bottom border on New Card Based on Priority
 function priorityCheck(value,card){
-    if(value == 1 || value == 2 ){
-        card.style.border = "2px solid red";
+    if(value == 1 ){
+        card.setAttribute('id','top');
+    }
+    else if(value == 2){
+        card.setAttribute('id','middle');
     }
     else if(value == 3){
-        card.style.border = "2px solid orange";
-    }
-    else if(value == 4 || value == 5){
-        card.style.border = "2px solid green";
+        card.setAttribute('id','low');
     }
     else{
         console.log("Nothing");
     }
 }
 
+// Grabs Today's Date
 function postDate(){
     const date = new Date();
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
-    let currentDate = `${month}-${day}-${year}`;
+    let currentDate = "";
+
+    if(month < 10){
+     currentDate = `${year}-0${month}-${day}`;
+    }
+    else{
+        currentDate = `${year}-${month}-${day}`;
+    }
     return currentDate;
 }
 
-console.log(postDate());
+function darkMode(){
+    let page = document.body;
+    let card = document.querySelector(".newCard");
+    page.classList.toggle("dark-mode");
+    card.classList.toggle("newCardDark");
+}
